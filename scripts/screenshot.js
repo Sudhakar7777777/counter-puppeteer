7c96e0123b804e9393dafe5ad20d2105a7dd8b39
Sudhakar Balakrishnan <>
2020-12-20 16:19:12 -0800

    add pdf + login example

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -1,5 +1,32 @@
 const puppeteer = require('puppeteer')
 
+const fbSign = async (page) => {
+	console.log('Starting new session')
+	let SECRET_EMAIL = 'funny@email.com'
+	let SECRET_PASSWORD = 'pass'
+	// const page = await browser.newPage()
+	await page.goto('https://facebook.com', { waitUntil: 'networkidle2' })
+	await page.waitForSelector('._featuredLogin__formContainer')
+	await page.type('input#email', SECRET_EMAIL)
+	await page.type('input#pass', SECRET_PASSWORD)
+	await page.click('button[name="login"]')
+	console.log('FB click done')
+}
+
+const screenshotPDF = async (page) => {
+	// const page = await browser.newPage()
+	await page.goto('https://www.vudu.com/content/movies/uxrow/4K-UHD/74?offset=1200', { waitUntil: 'networkidle2' })
+	await page.waitFor(10000);
+	await page.pdf({ path: 'vudu-4k-movies.pdf', format: 'A4' })
+}
+
+const screenshotPNG = async (page) => {
+	// const page = await browser.newPage()
+	await page.goto('https://www.vudu.com/content/movies/uxrow/New-Rentals/53', { waitUntil: 'networkidle2' })
+	await page.waitForSelector('._2CCgL');        // wait for the selector to load
+	await page.screenshot({ path: 'vudu-new-movies.png', fullPage: true });
+}
+
 const main = async () => {
 	const browser = await puppeteer.launch({
 		args: ['--start-maximized'],
@@ -7,24 +34,17 @@ const main = async () => {
 		defaultViewport: null
 	})
 	const page = await browser.newPage()
-	await page.setViewport({
-		width: 1920,
-		height: 1080,
-		deviceScaleFactor: 1,
-	})
-	await page.goto('https://www.vudu.com/content/movies/uxrow/New-Rentals/53')
 
-	// await page.waitForNavigation({ waitUntil: 'networkidle0' })
-	// await page.waitForNavigation()
-	await page.waitFor(10000);
+	try {
+		await screenshotPNG(page);
+		// await screenshotPDF(page);
+		await fbSign(page);
+	} catch (err) {
+		console.log("SCRIPT ERROR: (" + err.name + " - " + err.message + ")");
+		console.log(err);
+	}
 
-	await page.waitForSelector('._2CCgL');        // wait for the selector to load
-	// const movies = await page.$eval("._3WIR9", e => e.innerHTML);
-
-	// Take the screenshot
-	await page.screenshot({ path: 'vudu-new-movies.png', fullPage: true });
-	// await page.screenshot({ path: 'vudu-new-movies.jpeg', fullPage: true, type: 'jpeg', quality: 90 });
-
+	//clean up
 	await browser.close()
 }
 
